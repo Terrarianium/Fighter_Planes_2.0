8440791ab8e09c857c1bfb83a6ecae4a582fafb2
Terrarianium <Collinrogers915@gmail.com>
2024-11-07 15:10:27 -0500

    Added Game
    Added the game to the repository.

---
+++ Fighter Planes F24/Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    private float timeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, 5);
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit) 
+    {
+        if (whatDidIHit.tag == "Player")
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
