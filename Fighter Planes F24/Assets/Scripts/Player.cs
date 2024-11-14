7ffcfdb0b2dbaabafb00297dbbb289db506d9854
Terrarianium <Collinrogers915@gmail.com>
2024-11-14 16:09:38 -0500

    Week 13 Update
    Updated most things with the resources on Canvas and according to instructions.

--- Fighter Planes F24/Assets/Scripts/Player.cs
+++ Fighter Planes F24/Assets/Scripts/Player.cs
@@ -9,23 +9,32 @@ public class Player : MonoBehaviour
 
     private float horizontalInput;
     private float verticalInput;
+    private float horizontalScreenSize = 11.5f;
+    private float verticalScreenSize = 6f;
     private float speed;
-    private float horizontalScreenLimit;
-    private float verticalScreenLimit;
-
-    public GameObject explosion;
-    public GameObject bullet;
     private int lives;
-    public TextMeshProUGUI lifeText;
+    private int shooting;
+    private bool hasShield;
+
+    public GameManager gameManager;
+
+    public GameObject bullet;
+    public GameObject explosion;
+    public GameObject thruster;
+    public GameObject shield;
+
+    public TextMeshProUGUI livesText;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 6f;
-        horizontalScreenLimit = 11.5f;
-        verticalScreenLimit = 7.5f;
         lives = 3;
-        lifeText.text = "Lives: " + lives;
+        shooting = 1;
+        hasShield = false;
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        livesText = GameObject.Find("Lives").GetComponent<TMPro.TextMeshProUGUI>();
+        livesText.text = "Lives: " + lives;
     }
 
     // Update is called once per frame
@@ -39,35 +48,116 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        transform.Translate(new Vector3(horizontalInput, verticalInput,0) * Time.deltaTime * speed);
+        if (transform.position.x > horizontalScreenSize || transform.position.x <= -horizontalScreenSize)
         {
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
+        if (transform.position.y > 0f)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        if (transform.position.y < -verticalScreenSize)
+        {
+            transform.position = new Vector3(transform.position.x, -verticalScreenSize, 0);
         }
     }
 
     void Shooting()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+            switch (shooting)
+            {
+                case 1:
+                    Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.identity);
+                    Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.Euler(0, 0, 30f)); 
+                    Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+                    Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.Euler(0, 0, -30f));
+                    break;
+            }
         }
     }
 
     public void LoseALife()
     {
-        //lives = lives - 1;
-        //lives -= 1;
-        lives--;
+        if (hasShield == false)
+        {
+            lives--;
+            livesText.text = "Lives: " + lives;
+        } else if (hasShield == true)
+        {
+            shield.gameObject.SetActive(false);
+            hasShield = false;
+            gameManager.PlayPowerDown();
+        }
+
         if (lives == 0)
         {
+            gameManager.GameOver();
             Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
-        lifeText.text = "Lives: " + lives;
+    }
+
+    IEnumerator SpeedPowerDown()
+    {
+        yield return new WaitForSeconds(3f);
+        speed = 6f;
+        thruster.gameObject.SetActive(false);
+        gameManager.UpdatePowerupText("");
+        gameManager.PlayPowerDown();
+    }
+
+    IEnumerator ShootingPowerDown()
+    {
+        yield return new WaitForSeconds(3f);
+        shooting = 1;
+        gameManager.UpdatePowerupText("");
+        gameManager.PlayPowerDown();
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatIHit)
+    {
+        if(whatIHit.tag == "Powerup")
+        {
+            gameManager.PlayPowerUp();
+            int powerupType = Random.Range(1, 5); //this can be 1, 2, 3, or 4
+            switch(powerupType)
+            {
+                case 1:
+                    //speed powerup
+                    speed = 9f;
+                    gameManager.UpdatePowerupText("Picked up Speed!");
+                    thruster.gameObject.SetActive(true);
+                    StartCoroutine(SpeedPowerDown());
+                    break;
+                case 2:
+                    //double shot
+                    shooting = 2;
+                    gameManager.UpdatePowerupText("Picked up Double Shot!");
+                    StartCoroutine (ShootingPowerDown());
+                    break;
+                case 3:
+                    //triple shot
+                    shooting = 3;
+                    gameManager.UpdatePowerupText("Picked up Triple Shot!");
+                    StartCoroutine(ShootingPowerDown());
+                    break;
+                case 4:
+                    //shield
+                    gameManager.UpdatePowerupText("Picked up Shield!");
+                    shield.gameObject.SetActive(true);
+                    hasShield = true;
+                    break;
+            }
+            Destroy(whatIHit.gameObject);
+        }
     }
 }
